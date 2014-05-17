af500ce00e6241f7d9ba98b3e70f515942dd11f9
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-05-17 12:53:17 +0200

    replaced inline api constants with the right variables

--- WebSocket.js
+++ WebSocket.js
@@ -355,7 +355,7 @@ Ext.define('Ext.ux.data.proxy.WebSocket', {
                 return false;
             }
 
-            if (action === 'update') {
+            if (action === me.getApi().update) {
                 for (var i = 0; i < resultSet.records.length; i++) {
                     var record = store.getById(resultSet.records[i].internalId);
 
@@ -366,7 +366,7 @@ Ext.define('Ext.ux.data.proxy.WebSocket', {
 
                 store.commitChanges();
             }
-            else if (action === 'destroy') {
+            else if (action === me.getApi().destroy) {
                 store.remove(resultSet.records);
             }
             else {
